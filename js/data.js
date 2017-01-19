329c7359bea0ddae73f75deff2c244b16cefba70
Abel-Johnson <734157604@qq.com>
2017-01-19 23:52:15 +0800

    界面做好了
    明天开始做逻辑以及数据

---
+++ js/data.js
@@ -0,0 +1,43 @@
+var demoLis = [
+	{
+		id: 1,
+		title: '系列(1)',
+		type: 'folder'
+	},
+	{
+		id: 2,
+		title: '系列(2)',
+		type: 'folder'
+	},
+	{
+		id: 3,
+		title: '系列(3)',
+		type: 'folder'
+	},
+	{
+		id: 2,
+		title: 'demo1',
+		type: 'demo'
+	},
+	{
+		id: 2,
+		title: 'demo2',
+		type: 'demo'
+	},
+	{
+		id: 3,
+		title: 'demo3',
+		type: 'demo'
+	},
+	{
+		id: 4,
+		title: 'demo4',
+		type: 'demo'
+	},
+	{
+		id: 5,
+		title: 'demo5',
+		type: 'demo'
+	}
+]
+	

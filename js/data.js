90d121ce81cf529f5baa787330411e4f4d48904a
Abel-Johnson <734157604@qq.com>
2017-01-28 16:46:51 +0800

    2.28

--- js/data.js
+++ js/data.js
@@ -1,10 +1,10 @@
 var skillLis = {
-	temp:"circle",
-	skillsClassify:[
+	showType:"circle",
+	content:[
 	{
-		skillLanguage:"html5",
+		language:"html5",
 		percent:"6%",
-		skillTooltip:[
+		tips:[
 			"熟练掌握各类语义化标签",
 			"熟悉对各种标签特性及其相互转换",
 			"H5拖拽、视频音频",
@@ -12,9 +12,9 @@ var skillLis = {
 		]
 	},
 	{
-		skillLanguage:"CSS3",
+		language:"CSS3",
 		percent:"90%",
-		skillTooltip:[
+		tips:[
 			"掌握浮动及文档流特性",
 			"精通定位、浏览器兼容性",
 			"CSS3圆角阴影字体等样式有实际案例经验",
@@ -22,9 +22,9 @@ var skillLis = {
 		]
 	},
 	{
-		skillLanguage:"javascript",
+		language:"javascript",
 		percent:"86%",
-		skillTooltip:[
+		tips:[
 			"了解数据类型、作用域闭包等语言特性",
 			"掌握定时器、数组字符串及递归、数组去重等",
 			"深入使用DOM\BOM\EVENT，能完成各类组件开发",
@@ -34,66 +34,267 @@ var skillLis = {
 		]
 	},
 	{
-		skillLanguage:"canvas",
+		language:"canvas",
 		percent:"40%",
-		skillTooltip:[
+		tips:[
 			"能熟练使用相关接口绘制各类图形",
 			"能运用canvas开发小游戏",
 			"可封装小型图表等数据可视化技术解决方案"
 		]
 	},
 	{
-		skillLanguage:"jQuery",
+		language:"jQuery",
 		percent:"60%",
-		skillTooltip:[
+		tips:[
 			"移动端响应式",
 			"canvas游戏开发"
 		]
-	}]
+	}
+	]
 }
 
 
+var cases = [
+		{
+			caseName:"event",
+			caseList:[
+				{
+					caseTitle:"手风琴",
+					publishTime:"2016-09-26",
+					caseThumbnail:"images/case/1/1-JS热身课程-课后练习.gif",
+					caseDescription:"用到了事件的知识点用到了事件的知识点用到了事件的知识点用到了事件的知识点用到了事件的知识点",
+					caseWebsite:"cases/event/shoufengqin/手风琴.html"
+				},
+				{
+					caseTitle:"zhaopianqiang",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/case/1/2-模拟手机短信发送.gif",
+					caseDescription:"描述：这是我完成的一个小案例，综合涉及到的知识点是判断、字符串拼接，以及布局方面的小技巧，比如提交留言后，A用户在左，B用户在右"
+					,caseWebsite:"http:www.miaov.com"
+				}
+			]
+		},
+		{
+			caseName:"for应用this关键字",
+			caseList:[
+				{
+					caseTitle:"上下移动",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏览器<br/>可以被<br/>认为浏<br/>览<br/>器<br/>可<br/>以<br/>浏览器<br/>可以被<br/"
+					,caseWebsite:"http:www.miaov.com"
+				},
+				{
+					caseTitle:"切换来去",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"http://jquery.com/jquery-wp-content/themes/jquery.com/i/try-jquery.jpg",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认"
+					,caseWebsite:"http:www.miaov.com"
+				},
+				{
+					caseTitle:"切换来去",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"http://jquerymobile.com/resources/devices.png",
+					caseDescription:"为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				},{
+					caseTitle:"上下移动",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"https://avatars1.githubusercontent.com/u/6025224?v=3&s=400",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				},
+				{
+					caseTitle:"切换来去",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"认为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				},{
+					caseTitle:"上下移动",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				},
+				{
+					caseTitle:"切换来去",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				},{
+					caseTitle:"上下移动",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				},
+				{
+					caseTitle:"切换来去",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				}
+			]
+		},{
+			caseName:"自定义属性、索引值",
+			caseList:[
+				{
+					caseTitle:"图片轮播图1",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:" liulanqi 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
 
-var demoLis = [
-	{
-		id: 1,
-		title: '系列(1)',
-		type: 'folder'
-	},
-	{
-		id: 2,
-		title: '系列(2)',
-		type: 'folder'
-	},
-	{
-		id: 3,
-		title: '系列(3)',
-		type: 'folder'
-	},
-	{
-		id: 2,
-		title: 'demo1',
-		type: 'demo'
-	},
-	{
-		id: 2,
-		title: 'demo2',
-		type: 'demo'
-	},
-	{
-		id: 3,
-		title: 'demo3',
-		type: 'demo'
-	},
-	{
-		id: 4,
-		title: 'demo4',
-		type: 'demo'
-	},
-	{
-		id: 5,
-		title: 'demo5',
-		type: 'demo'
-	}
+				},
+				{
+					caseTitle:"图片轮播图2",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com",
+
+				}
+			]
+		},
+		{
+			caseName:"JS数据类型、类型转换",
+			caseList:[
+				{
+					caseTitle:"上下移动",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				},
+				{
+					caseTitle:"切换来去",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				}
+			]
+		},{
+			caseName:"函数传参、重用、价格计算",
+			caseList:[
+				{
+					caseTitle:"图片轮播图1",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:" liulanqi 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+
+				},
+				{
+					caseTitle:"图片轮播图2",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com",
+
+				}
+			]
+		},
+		{
+			caseName:"运算符流程控制",
+			caseList:[
+				{
+					caseTitle:"上下移动",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				},
+				{
+					caseTitle:"切换来去",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				}
+			]
+		},{
+			caseName:"定时器管理、函数封装",
+			caseList:[
+				{
+					caseTitle:"图片轮播图1",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:" liulanqi 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+
+				},
+				{
+					caseTitle:"图片轮播图2",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com",
+
+				}
+			]
+		},
+		{
+			caseName:"日期对象、时钟倒计时",
+			caseList:[
+				{
+					caseTitle:"上下移动",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				},
+				{
+					caseTitle:"切换来去",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/icon/exp.png",
+					caseDescription:"浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认 为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可 以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为浏览器可以被认为 浏览器可以被认为浏览器可以被认为"
+					,caseWebsite:"http:www.miaov.com"
+				}
+			]
+		},{
+			caseName:"字符串、查找高亮显示",
+			caseList:[
+				{
+					caseTitle:"消除表情小游戏",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/case/1/消除表情小游戏.png",
+					caseDescription:"这是一个综合小练习，是运用JS的相关方法以及一些逻辑来实现的小游戏，可玩性极高哦~"
+					,caseWebsite:"http://bbs.miaov.com/online_class/JS1-lesson-gif/JS37.html"
+
+				},
+				{
+					caseTitle:"排序实例",
+					publishTime:"2011-11-16 22:08",
+					caseThumbnail:"images/case/1/排序.png",
+					caseDescription:"数组的方法sort实在妙不可言，通过各种小案例来达到令人满意的效果，也是学以致用的一种体验。"
+					,caseWebsite:"http://bbs.miaov.com/online_class/JS1-lesson-gif/JS38.png",
+
+				}
+			]
+		}
 ]
-	
+
+var projects = [
+		{
+			projectName:"整屏切换+动画制作“TweenMax库”",
+			projectWebsite:"http://2013.miaov.com/miaov_demo/diagnosite/",
+		    startTime:"2016.01.01",
+			endTime:"至今",
+			projectExplain:"JS整站开发之：整屏切换+动画制作神器“TweenMax库” 导航条和首屏出场动画、3D翻转实现、整屏切换，页面动画计算滚动条滚动距离， 拖动滚动条触发页面动画，完善每一屏内的图片翻转，缩小浏览器，做布局转换。",
+			projectLabel:["html","css3","css","js"],
+			projectThumbnail:"images/icon/1.png"
+		},
+		{
+			projectName:"妙味VIP会员感恩墙",
+			projectWebsite:"http://v.miaov.com/thanks/",
+		    startTime:"2016.01.01",
+			endTime:"至今",
+			projectExplain:"用户观看视频的主页面，其大部分内容是通过JS加载的。为了提高性能，采用了延迟加载、延迟渲染、事件代理等优化方式。此外，还通过监听页面的window.onerror事件对异常信息进行采集，以便及时发现和排查故障。",
+			projectLabel:["html","css3","css","js"],
+			projectThumbnail:"images/icon/project-2.png"
+		}
+]
--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 var rad = Math.PI/180;
 var circle = $('#circle')[0];
 var ctx = circle.getContext("2d");
-circles(0,2*Math.PI);
+
+
+//circles(0,2*Math.PI);
+//动画生成同心圆
+prepare(100, -90, 3)
 function circles(start,end) {
+//	console.log(ctx);
 	ctx.strokeStyle = "#eef2f5";
 	ctx.lineWidth = 2;
 	ctx.moveTo(294,294);
@@ -83,7 +88,7 @@ function prepare(percent, startDeg, time) {
 		var start = startDeg*rad;
 		var end = startDeg*rad+percent/100*2*Math.PI/100*(++i)
 		
-		console.log(start,end);
+//		console.log(start,end);
 		if(end >= start+2*Math.PI*percent/100){
 			clearInterval(timer);
 		}
@@ -115,7 +120,7 @@ function dataShow(radius, percent, startDeg, time, color) {
 		
 		ctx.beginPath()
 		ctx.moveTo(294,294);
-		ctx.fillStyle = transparent;
+		ctx.fillStyle = 'transparent';
 		ctx.arc(294,294,radius-30,start,end,false)
 		ctx.stroke();
 		ctx.fill();
@@ -123,9 +128,221 @@ function dataShow(radius, percent, startDeg, time, color) {
 }
 
 
-$(document).click(function(){
+//$(document).click(function(){
+//	console.log("进入");
 //	setTimeout(function(){
 //		dataShow(290,90,270,1000,"#fffff0");
 //		dataShow(260,80,270,1000,"#ffff00");
 //	},2000)
+//});
+
+
+
+//vue写技能部分
+new Vue({
+	el: '.word',
+	data: {
+		list: skillLis.content,//获取到技能数组
+		showPageIndex: 0
+	},
+	computed: {
+		pageNum: function(){
+			return Math.ceil(this.list.length/3)
+		}
+	},
+	methods: {
+		changePage: function(index) {
+			this.showPageIndex = index;
+		}
+	}
 });
+
+//vue写项目部分
+new Vue({
+	el: '.ps_content',
+	data: {
+		list: projects//获取到proj数组
+	},
+	computed: {},
+	methods: {
+		
+	}
+})
+
+//vue写demo部分
+
+//给每一项数据加一个表征当前显示状态的属性
+for (var i = 0; i < cases.length; i++) {
+	cases[i].isOpen = false;
+}
+cases[0].isOpen = true;
+console.log(cases);
+
+
+
+let vm = new Vue({
+	el: '#demosShow',
+	data: {
+		list: cases,//获取到demo数组
+		currentCaseObj: cases[0].caseList[0]
+	},
+	methods: {
+		showDetail: function(item) {
+			this.currentCaseObj = item;
+			
+			this.$nextTick(function() {
+//				var innerObj = vm.$refs['demoDesc'];//获取到渲染完的描述部分
+//				console.log("获取到innerObj");
+//				var rateR = $('.container').height()/$(innerObj).outerHeight()
+////				console.log($('.container').height(),$(innerObj).outerHeight(), innerObj.offsetHeight);
+//				if(rateR>=1) {
+					sWay.eq(1).hide()
+//				} else {
+//					sWay.eq(1).show()
+//				};
+//				if(rateR<1/20) rateR=1/20;
+//				console.log(rateR);
+//				sDiv.eq(1).height(rateR*sWay.height())
+			})
+		},
+		imgFinish: function() {
+			console.log("图片加载完毕");
+			var innerObj = vm.$refs['demoDesc'];//获取到渲染完的描述部分
+//				console.log(innerObj);
+				var rateR = $('.container').height()/$(innerObj).outerHeight()
+//				console.log($('.container').height(),$(innerObj).outerHeight(), innerObj.offsetHeight);
+				if(rateR>=1) {
+					sWay.eq(1).hide()
+				} else {
+					sWay.eq(1).show()
+				};
+				if(rateR<1/20) rateR=1/20;
+				console.log(rateR);
+				sDiv.eq(1).height(rateR*sWay.height())
+		},
+		switchFolder: function(item,index) {
+//			this.currentFolderObj = item;
+			console.log(index);
+			for (var i = 0; i < cases.length; i++) {
+				if(i!== index){
+					cases[i].isOpen = false;
+				}
+			}
+			item.isOpen = !item.isOpen;
+			
+			this.$nextTick(function() {
+				var innerObj = vm.$refs['folderLis'];//获取到渲染完的ul
+				var rateL = $('.container').height()/$(innerObj).height()//
+				console.log('窗口高度/内容高度的比例'+rateL);
+				if(rateL>=1) sWay.eq(0).hide();//如果内容不够多,就让滚动条消失
+				if(rateL<1/20) rateL=1/20;//如果内容太多,就让滚动条保持在1/20的大小
+				sDiv.eq(0).height(rateL*sWay.height())//计算出此时的滑块高度
+				
+				var maxL = $(innerObj).height()-$('.container').height();
+				var maxDL = sWay.eq(0).height() - sDiv.eq(0).height()
+				console.log('内容高度:'+$(innerObj).height(),'内容top值:'+$(innerObj).position().top,'容器高度'+$('.container').height());
+				if( $(innerObj).height() + $(innerObj).position().top < $('.container').height()) {
+					$(innerObj).css('top', -($(innerObj).height() - $('.container').height())+"px" )
+					console.log("底部靠上了");
+				}
+				
+				
+				var rateNow = -($(innerObj).position().top/maxL);
+				console.log('内容区top距离值'+$(innerObj).position().top,'内容区最大滚动距离'+maxL);
+				console.log('比例'+rateNow);
+				var divTop = rateNow*maxDL;
+				sDiv.eq(0).css('top',divTop+"px");
+			})
+		}
+	}
+});
+
+
+
+
+//写滚动条部分
+//container: 窗口容器
+$('container')
+//folderLis/demoDesc: 内容
+$('.folderLis')
+$('.demoDesc')
+//scrollBar: 滑槽
+var sWay = $('.scrollBar')
+//barDiv: 滑块
+var sDiv = $('.barDiv')
+
+//先写左边列表区的滑块对应逻辑
+var rateL = $('.container').height()/$('.folderLis').height()
+if(rateL>=1) sWay.eq(0).hide();
+if(rateL<1/20) rateL=1/20;
+sDiv.eq(0).height(rateL*sWay.height())
+
+
+
+var rateR = $('.container').height()/$('.demoDesc').height()
+if(rateR>=1) sWay.eq(1).hide();
+if(rateR<1/20) rateR=1/20;
+sDiv.eq(1).height(rateR*sWay.height())
+
+
+
+//添加滑块拖动逻辑
+
+//右边
+
+//左边
+
+
+
+sDiv.eq(0).on('mousedown', function(ev) {
+	ev.preventDefault()//添加处理,不让鼠标拖动时选中文字
+	//左边
+	var maxL = $('.folderLis').height()-$('.container').height();
+	var maxDL = sWay.eq(0).height() - sDiv.eq(0).height()
+	var dis = ev.clientY- $(this).position().top;
+	function mdFn(ev) {
+		//滑块定位
+//		console.log(ev.clientY);
+//		滑块top值
+		var divTop = ev.clientY-dis;
+		
+		if(divTop<0) divTop = 0;
+		if(divTop>maxDL) divTop = maxDL;
+		
+		sDiv.eq(0).css('top',divTop+"px");
+		$('.folderLis').css('top',-divTop/maxDL*maxL+"px");
+	}
+	
+	$(document).on('mousemove', mdFn)
+	$(document).on('mouseup', function() {
+		$(document).off('mousemove', mdFn)
+	})
+})
+
+
+sDiv.eq(1).on('mousedown', function(ev) {
+	ev.preventDefault()//添加处理,不让鼠标拖动时选中文字
+	var maxR = $('.demoDesc').innerHeight()-$('.container').eq(1).height();
+	console.log($('.demoDesc').height(),$('.container').eq(1).height());
+	var maxDR = sWay.eq(1).height() - sDiv.eq(1).height()
+	console.log(maxR,maxDR);
+	var dis = ev.clientY- $(this).position().top;
+	function mdFn(ev) {
+		//滑块定位
+		
+//		给滑块定位
+		var divTop = ev.clientY-dis;
+		if(divTop<0) divTop = 0;
+		if(divTop>maxDR) divTop = maxDR;
+		console.log(divTop);
+		sDiv.eq(1).css('top',divTop+"px");
+		console.log(divTop/maxDR*maxR);
+		$('.demoDesc').css('top',-divTop/maxDR*maxR+"px");
+	}
+	
+	$(document).on('mousemove', mdFn)
+	$(document).on('mouseup', function() {
+		$(document).off('mousemove', mdFn)
+	})
+})
+
